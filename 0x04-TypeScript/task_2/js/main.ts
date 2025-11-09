5dfce97105cacc677a19cd2677bf7b5a41b68816
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-11-09 14:30:11 +0100

    Add isDirector type guard and executeWork function

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,60 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// ✅ Execute work
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+// Example usage
+const emp1 = createEmployee(200); // Teacher
+const emp2 = createEmployee(1000); // Director
+const emp3 = createEmployee("600"); // Director
